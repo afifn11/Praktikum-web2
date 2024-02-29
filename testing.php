0d51865bdfb79636dd59db3234adb9297c856ac0
Muhammad Afif Naufal <muhammadafifnaufal3@gmail.com>
2024-02-29 17:00:58 +0700

    menambahkan folder praktikum 1

---
+++ testing.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+</head>
+
+<body>
+      <?php
+      $nama = "Muhammad Afif Naufal";
+      $prodi = "Teknik Informatika";
+      $angkatan = 2023;
+
+      echo "hello, saya $nama <br/>";
+      echo 'saya mahasiswa program studi ' . $prodi . ' angkatan tahun ' . $angkatan
+      
+      ?>
+
+      <div>
+            <h1> </h1>
+
+      </div>
+</body>
+
+</html>
---
+++ variabel_user.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Praktikum 1</title>
+</head>
+<body>
+    <?php
+    $nama = 'Muhammad Afif Naufal';
+    $umur = 19;
+    $berat = 57;
+
+    echo 'Nama : ' . $nama;
+    echo '<br/> Umur : ' . $umur . ' Tahun';
+    echo '<br/> Berat : '. $berat. ' Kg';
+
+    echo "<br/>Hello $nama Apakabar";
+    ?>
+</body>
+</html>
